12fc78025a3451b929bd2bbc98a8c6f1660dd7b8
Efe Çankaya <efecankaya@sabanciuniv.edu>
2022-05-15 11:29:37 +0300

    add sign up view

--- lib/routes/signup_view.dart
+++ lib/routes/signup_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:penta/util/colors.dart';
 import 'package:penta/util/styles.dart';
+import 'package:penta/util/screenSizes.dart';
 
 class SignUpView extends StatefulWidget {
   @override
@@ -21,6 +22,134 @@ class _SignUpViewState extends State<SignUpView> {
         backgroundColor: AppColors.primary,
         centerTitle: true,
         elevation: 0.0,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Image.asset(
+              'assets/icons/penta-transparent.png',
+              width: 250,
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding:
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      width: screenWidth(context, dividedBy: 1.25),
+                      decoration: BoxDecoration(
+                        color: AppColors.primaryLight,
+                        borderRadius: BorderRadius.circular(29),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Username",
+                          icon: Icon(
+                            Icons.person,
+                            color: AppColors.primary,
+                          ),
+                          hintStyle: TextStyle(color: AppColors.primary),
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding:
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      width: screenWidth(context, dividedBy: 1.25),
+                      decoration: BoxDecoration(
+                        color: AppColors.primaryLight,
+                        borderRadius: BorderRadius.circular(29),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Your Email",
+                          icon: Icon(
+                            Icons.email,
+                            color: AppColors.primary,
+                          ),
+                          hintStyle: TextStyle(color: AppColors.primary),
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding:
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      width: screenWidth(context, dividedBy: 1.25),
+                      decoration: BoxDecoration(
+                        color: AppColors.primaryLight,
+                        borderRadius: BorderRadius.circular(29),
+                      ),
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                          icon: Icon(
+                            Icons.lock,
+                            color: AppColors.primary,
+                          ),
+                          hintStyle: TextStyle(color: AppColors.primary),
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: screenWidth(context, dividedBy: 1.25),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(29),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: AppColors.primary,
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(vertical: 16),
+                            child: Text(
+                              "Sign Up",
+                              style: kButtonDarkTextStyle,
+                            ),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
